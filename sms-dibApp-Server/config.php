87ca3aea4635c106eed6a73bff4a7cdbdb50bdf9
Francesco Peragine <francescoperagine@gmail.com>
2019-07-25 05:49:29 +0000

    Configuration file

---
+++ sms-dibApp-Server/config.php
@@ -0,0 +1,16 @@
+<?php
+define("LOGIN_OK_CODE", 201);
+define("LOGIN_OK_TEXT", "Login successful.");
+define("REGISTER_OK_CODE", 202);
+define("REGISTER_OK_TEXT", "Registration successful.");
+define("USER_CREATED_CODE", 203);
+define("USER_CREATED_TEXT", "User exists");
+define("INVALID_PASSWORD_CODE", 301);
+define("INVALID_PASSWORD_TEXT", "Invalid password.");
+define("INVALID_EMAIL_CODE", 303);
+define("INVALID_EMAIL_TEXT", "Email not registered.");
+define("MISSING_MANDATORY_PARAMETERS_CODE", 304);
+define("MISSING_MANDATORY_PARAMETERS_TEXT", "Missing mandatory parameters.");
+define("USER_ALREADY_EXISTS_CODE", 305);
+define("USER_ALREADY_EXISTS_TEXT", "User exists");
+?>
