b00b7189c17904e01323d4adbc366f4a0d237b88
Francesco Peragine <francescoperagine@gmail.com>
2019-10-05 13:32:47 +0200

    fix onBackPressed from login activity & splash intent

--- app/src/main/java/it/teamgdm/sms/dibapp/LoginActivity.java
+++ app/src/main/java/it/teamgdm/sms/dibapp/LoginActivity.java
@@ -133,4 +133,11 @@ public class LoginActivity extends BaseActivity {
         }
         return false;
     }
+
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
 }
